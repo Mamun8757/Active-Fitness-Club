389ea244e8e1933fd3bc781d69da49b137bad492
Mamun8757 <almamun8757@gmail.com>
2022-09-29 12:49:26 +0600

    title and main components created

---
+++ src/Components/Main/Main.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './Main.css'
+const Main = () => {
+    return (
+        <div className='main-components'>
+            <div>
+                <h2>Activity card Here</h2>
+            </div>
+            <div>
+                <h2>Activity Data List</h2>
+            </div>
+        </div>
+        
+    );
+};
+
+export default Main;
---
+++ src/Components/Title/Title.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import './Title.css'
+const Title = () => {
+    return (
+        <div className='header-title'>
+            <h2>ACTIVE-FITNESS-CLUB</h2>
+        </div>
+    );
+};
+
+export default Title;
